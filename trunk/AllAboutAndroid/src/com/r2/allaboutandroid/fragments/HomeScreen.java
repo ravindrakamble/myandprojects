fcad839512fd19a3dd6597c6b53c687a70828c27
ravindrakamble4u@gmail.com <ravindrakamble4u@gmail.com@77548c2d-6ab4-a4de-ed14-4a767fbfdbb4>
2014-06-06 13:09:20 +0000

    added homescreen

---
+++ trunk/AllAboutAndroid/src/com/r2/allaboutandroid/fragments/HomeScreen.java
@@ -0,0 +1,9 @@
+package com.r2.allaboutandroid.fragments;
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+
+@SuppressLint("NewApi")
+public class HomeScreen extends Fragment {
+
+}
